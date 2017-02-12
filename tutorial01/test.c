4ee9fe7b76f4b9912c137c7ed71fab91e7df7031
Rui Guo <rguo@leancloud.rocks>
2017-02-12 11:30:01 +0800

    (fix) root not singular

--- tutorial01/test.c
+++ tutorial01/test.c
@@ -72,11 +72,11 @@ static void test_parse_root_not_singular() {
 
     v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR, lept_parse(&v, "true x"));
-    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+    EXPECT_EQ_INT(LEPT_TRUE, lept_get_type(&v));
 
     v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR, lept_parse(&v, "false x"));
-    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+    EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
 }
 
 static void test_parse() {
