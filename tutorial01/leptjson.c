c5b4d2556357eb4547cad03992a8729025ec3abf
Rui Guo <rguo@leancloud.rocks>
2017-02-12 11:20:11 +0800

    (feat) tutorial101

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>  /* NULL */
 
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
+#define CHECK_SINGULARITY(c) \
+    do {\
+        if (c->json[0] != '\0')\
+            return LEPT_PARSE_ROOT_NOT_SINGULAR;\
+    } while(0)
 
 typedef struct {
     const char* json;
@@ -20,13 +25,36 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
         return LEPT_PARSE_INVALID_VALUE;
     c->json += 3;
+    CHECK_SINGULARITY(c);
     v->type = LEPT_NULL;
+    return LEPT_PARSE_OK;
+}
+
+static int lept_parse_true(lept_context* c, lept_value* v) {
+    EXPECT(c, 't');
+    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 3;
+    CHECK_SINGULARITY(c);
+    v->type = LEPT_TRUE;
+    return LEPT_PARSE_OK;
+}
+
+static int lept_parse_false(lept_context* c, lept_value* v) {
+    EXPECT(c, 'f');
+    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 4;
+    CHECK_SINGULARITY(c);
+    v->type = LEPT_FALSE;
     return LEPT_PARSE_OK;
 }
 
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
         case 'n':  return lept_parse_null(c, v);
+        case 't':  return lept_parse_true(c, v);
+        case 'f':  return lept_parse_false(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
         default:   return LEPT_PARSE_INVALID_VALUE;
     }
